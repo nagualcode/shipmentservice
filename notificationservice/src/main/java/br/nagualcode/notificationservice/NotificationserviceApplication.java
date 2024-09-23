1d7e6baf7f83ccedf49111823a250a1fcbecf899
Fred <nagual@nagual.local>
2024-09-22 23:17:02 -0300

    auto

--- notificationservice/src/main/java/br/nagualcode/notificationservice/NotificationserviceApplication.java
+++ notificationservice/src/main/java/br/nagualcode/notificationservice/NotificationserviceApplication.java
@@ -1,9 +1,11 @@
 package br.nagualcode.notificationservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableFeignClients
 public class NotificationserviceApplication {
 
 	public static void main(String[] args) {
