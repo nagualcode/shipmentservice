b068f5e29fdaac2e11322565237d32eb71f4f7f2
Fred <nagual@nagual.local>
2024-09-24 00:52:11 -0300

    auto

--- userservice/src/main/java/br/nagualcode/userservice/UserserviceApplication.java
+++ userservice/src/main/java/br/nagualcode/userservice/UserserviceApplication.java
@@ -1,9 +1,11 @@
 package br.nagualcode.userservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableFeignClients
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
